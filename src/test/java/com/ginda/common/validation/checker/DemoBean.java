ed52405c0960a052762ac6d1dc87255ce20156d5
曾银田 <zengyintian@iot.chinamobile.com>
2016-07-22 15:41:25 +0800

    完善验证内容

--- src/test/java/com/ginda/common/validation/checker/DemoBean.java
+++ src/test/java/com/ginda/common/validation/checker/DemoBean.java
@@ -14,4 +14,20 @@ public class DemoBean {
     /* 密码 */
     @RequiredValidator(required = true, description = "密码不能为空")
     private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/test/java/com/ginda/common/validation/checker/DemoCheckerTest.java
+++ src/test/java/com/ginda/common/validation/checker/DemoCheckerTest.java
@@ -12,5 +12,11 @@ public class DemoCheckerTest {
     public void testValidate() throws Exception {
         DemoBean demoBean = new DemoBean();
         Assert.assertEquals(DemoChecker.validate(demoBean), "用户名不能为空");
+
+        demoBean.setUserName("userName");
+        Assert.assertEquals(DemoChecker.validate(demoBean), "密码不能为空");
+
+        demoBean.setPassword("password");
+        Assert.assertNull(DemoChecker.validate(demoBean));
     }
 }
